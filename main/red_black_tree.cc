d81576de1c3c50217408cbc3bc1b91bc11f937a7
yqd1992 <31188877+yqd1992@users.noreply.github.com>
2018-09-15 00:54:54 +0800

    react structure

--- main/red_black_tree.cc
+++ main/red_black_tree.cc
@@ -2,11 +2,14 @@
 #include "tree.h"
 
 int main() {
-  Tree tree;
-  Node node1;
-  node1.value = 100;
-  tree.AddNode(&node1);
-  Node* root = tree.get_root();
-  tree.Traversal(root);
-  return 0;
+    Tree tree;
+    Node node1;
+
+    node1.value = 100;
+    tree.AddNode(&node1);
+    Node *root = tree.get_root();
+
+    tree.Traversal(root);
+
+    return 0;
 }
--- tree/tree.h
+++ tree/tree.h
@@ -6,88 +6,84 @@
 using namespace std;
 
 struct Node {
-	int value;
-	struct Node *left_child, *right_child, *father;
+    int value;
+    struct Node *left_child, *right_child, *father;
 };
 
-/** rbtree */
 class Tree {
 public:
-	Tree();
-	~Tree();
+    Tree();
 
-	Node* get_root();
-	void set_root(Node* root);
-	void AddNode(Node* node);
-	void Traversal(Node* node);
+    ~Tree();
+
+    Node *get_root();
+
+    void set_root(Node *root);
+
+    void AddNode(Node *node);
+
+    void Traversal(Node *node);
 
 private:
-	Node* root;
+    Node *root;
 };
 
 Tree::Tree() {
-	root = NULL;// (node *)malloc(sizeof(struct node))
+    root = NULL;
 }
 
-Tree::~Tree() {
-	//free(root);
-}
-
-Node* Tree::get_root() {
-	if (NULL == root) {
-		return NULL;
-	}
-	return root;
+Node *Tree::get_root() {
+    if (NULL == root) {
+        return NULL;
+    }
+    return root;
 }
 
 void Tree::set_root(Node *r) {
-	root = r;
+    root = r;
 }
 
-void Tree::AddNode(Node* n) {
-	if (NULL == root) {
-		root = n;
-		root->father = NULL;
-		root->left_child = NULL;
-		root->right_child = NULL;
-		return;
-	}
+void Tree::AddNode(Node *n) {
+    if (NULL == root) {
+        root = n;
+        root->father = NULL;
+        root->left_child = NULL;
+        root->right_child = NULL;
+        return;
+    }
 
-	int value = n->value;
-	Node* parent = NULL;
-	Node* pointer = root;
-	do {
-		parent = pointer;
-		if (value < pointer->value) {
-			pointer = pointer->left_child;
-		}
-		else if (value > pointer->value) {
-			pointer = pointer->right_child;
-		}
-		else {
-			// existed
-			return;
-		}
-	} while (NULL != pointer);
+    int value = n->value;
+    Node *parent = NULL;
+    Node *pointer = root;
+    do {
+        parent = pointer;
+        if (value < pointer->value) {
+            pointer = pointer->left_child;
+        } else if (value > pointer->value) {
+            pointer = pointer->right_child;
+        } else {
+            // existed
+            return;
+        }
+    } while (NULL != pointer);
 
-	n->father = parent;
-	if (value < parent->value) {
-		parent->left_child = n;
-	}
-	else {
-		parent->right_child = n;
-	}
-	// totate
+    n->father = parent;
+    if (value < parent->value) {
+        parent->left_child = n;
+    } else {
+        parent->right_child = n;
+    }
+    // rotate
 }
 
-void Tree::Traversal(Node* node) {
-	if (NULL == node) {
-		return;
-	}
+void Tree::Traversal(Node *node) {
+    if (NULL == node) {
+        return;
+    }
 
-	cout << node->value << endl;
-	Traversal(node->left_child);
-	Traversal(node->right_child);
+    cout << node->value << endl;
+    Traversal(node->left_child);
+    Traversal(node->right_child);
 }
 
 #endif
